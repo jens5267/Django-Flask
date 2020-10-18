7be7f94fafbe7ce3789d8f389fbe3c97ec6bf4f1
jens <j.reichwein@energiepartners.com>
2020-10-18 18:27:25 +0200

    fixed serializer

--- backend/backendapi/api/serializers.py
+++ backend/backendapi/api/serializers.py
@@ -7,9 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
-        extra_kwargs= {'password': {'write_only': True, 'required': True}}
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            Token.objects.create(user=user)
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
